59266801c381af6f99015600dc81a1e6acc355a7
WagnerTerry <wagner.goncalves@calindra.com.br>
2021-04-29 19:22:18 -0300

    aula 03

---
+++ src/services/UsersService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class UsersService {
+    async create(email: string){
+        const usersRepository = getCustomRepository(UsersRepository)
+        // Verificar se o usuario existe
+
+        const usersExists = await usersRepository.findOne({
+            email
+        })
+
+        // Se existir retornar user
+
+        if (usersExists) {
+            return usersExists
+        }
+
+        // Se não existir, salvar no DB
+        const user = usersRepository.create({
+            email
+        })
+
+        await usersRepository.save(user)
+
+        return user
+        
+
+    }
+}
+
+export { UsersService}
